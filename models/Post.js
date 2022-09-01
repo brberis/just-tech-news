bd7ef488e9b0d4d3fefb0fdd193c80e7a7c7c7d6
Cristobal Barberis <cristobal@barberis.com>
2022-09-01 00:21:54 -0400

    post queries

--- models/Post.js
+++ models/Post.js
@@ -2,9 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Post model
-class Post extends Model {
-  
-}
+class Post extends Model {}
 
 // create fields/columns for Post model
 Post.init(
@@ -41,3 +39,5 @@ Post.init(
     modelName: 'post'
   }
 );
+
+module.exports = Post;
